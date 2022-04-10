eb1ca1e322e0be7dc00346bd579a02b57a0ad805
Chad Tao <ctao@populus-media.com>
2022-04-10 09:32:52 -1000

    added auto mode

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const boardX = 60;
-const boardY = 25;
+const boardX = 60, boardY = 25;
+const dirKeys = {
+    ArrowUp: [-1, 0],
+    ArrowDown: [1, 0],
+    ArrowLeft: [0, -1],
+    ArrowRight: [0, 1]
+};
 const el = tag => document.createElement(tag);
 const $ = tag => document.querySelector(tag);
 const $A = tag => [...document.querySelectorAll(tag)];
 const rand = n => Math.floor(Math.random() * n);
-const log = s => console.log(s);
 const board = [...Array(boardY)].map(a => ([...Array(boardX)].map(() => 0)));
 const snake = [[Math.round(boardY / 2), Math.round(boardX / 2)]];
 let speed = 0;
-
-//directions
-// N - -1,0 E - 0,1 S - 1,0 W - 0, -1
+let ateApple = false;
+let autoMode = false;
 let dir = [-1, 0];
-let moveQueue = null;
+let moveQueue, apple;
 
 
 const renderBoard = () => {
@@ -23,8 +26,7 @@ const renderBoard = () => {
         row.classList.add('row');
         a.forEach((_, x) => {
             const block = el('span');
-            block.setAttribute('data-x', x);
-            block.setAttribute('data-y', y);
+            block.setAttribute('id', `y${y}-x${x}`)
             row.appendChild(block);
             snake.some(([m, n]) => m === y && n === x) && block.classList.add('solid')
         });
@@ -34,38 +36,62 @@ const renderBoard = () => {
 };
 
 const dropApple = () => {
-    let apple = [rand(boardY), rand(boardX)];
+    apple = [rand(boardY), rand(boardX)];
     while (snake.some(([a, b]) => a === apple[0] && b === apple[1])) {
         apple = [rand(boardY), rand(boardX)]
     };
-    log($('.game').children[apple[0]].children[apple[1]]);
     const location = $('.game').children[apple[0]].children[apple[1]];
     location.textContent = '🍎';
 }
 
 const motion = () => {
     moveQueue && clearInterval(moveQueue);
-    moveQueue = setInterval(move, 1000 - speed);
+    moveQueue = setInterval(move, 250 - speed);
 }
 
 const move = () => {
     const [[y, x]] = snake;
-    snake.unshift([(y + dir[0] + boardY) % boardY, (x + dir[1] + boardX) % boardX]);
-    snake.splice(-1);
-    updateSnake()
+    autoMode && autoSteer();
+    const next = [(y + dir[0] + boardY) % boardY, (x + dir[1] + boardX) % boardX];
+    snake.unshift(next);
+    !ateApple && snake.splice(-1);
+    ateApple = false;
+    updateSnake();
+    if (apple[0] === next[0] && apple[1] === next[1]) {
+        $(`#y${apple[0]}-x${apple[1]}`).textContent = ''
+        dropApple();
+        ateApple = true;
+        speed += 10;
+        $('.score').textContent = speed;
+        motion();
+    }
 }
 
 const updateSnake = () => {
     $A('.solid').forEach(a => a.classList.remove('solid'));
-    log(snake)
-    snake.forEach(([y,x])=> {
-        const location = $('.game').children[y].children[x];
-        location.classList.add('solid')
-    })
+    snake.forEach(([y, x]) => $('.game').children[y].children[x].classList.add('solid'));
 }
 
-
+const autoSteer = () => {
+    const [aY, aX] = apple;
+    const [sY, sX] = snake[0]
+    if (aY !== sY) return changeDir({ key: aY < sY ? 'ArrowUp' : 'ArrowDown' });
+    if (aX !== sX) return changeDir({ key: aX < sX ? 'ArrowLeft' : 'ArrowRight' });
+}
 
 renderBoard();
 dropApple();
 motion();
+
+const changeDir = (e) => {
+    if (!dirKeys[e.key]) return
+    if (!(dirKeys[e.key][0] + dir[0]) || !(dirKeys[e.key][1] + dir[1])) return
+    dir = dirKeys[e.key];
+}
+
+document.addEventListener('keyup', changeDir);
+
+$("button").addEventListener("click", () => {
+    autoMode = !autoMode;
+    $(".auto").textContent = autoMode ? 'ON' : 'OFF'
+});
